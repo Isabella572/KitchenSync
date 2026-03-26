fa695bbfc03fb0182e1b6fc5072a49b3a0289fa2
Isabella572 <isabellaj.boutros@gmail.com>
2026-03-26 23:22:32 +0000

    remove large csv from tracking

--- entities/user.py
+++
@@ -1,8 +0,0 @@
-from entities.diet_requirements import Diet_Requirements
-
-
-class User:
-    def __init__(self, id: int, name: str, requirements: Diet_Requirements):
-        self.id = id
-        self.name = name
-        self.requirements = requirements
